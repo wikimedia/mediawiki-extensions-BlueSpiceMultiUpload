b9f32503b28a0a1c31e8ce31f128841dcc8a1ada
miriam schlindwein <schlindwein@hallowelt.com>
2022-06-30 13:23:32 +0200

    Prevent multi upload on specialpage upload
    ERM29003
    
    [4.2.x]
    
    Change-Id: Ibc339abde5915c5063f17ce2f80c5cd810ee3562

--- src/Hook/BeforePageDisplay/AddResources.php
+++ src/Hook/BeforePageDisplay/AddResources.php
@@ -2,6 +2,8 @@
 
 namespace BlueSpice\MultiUpload\Hook\BeforePageDisplay;
 
+use MediaWiki\MediaWikiServices;
+
 class AddResources extends \BlueSpice\Hook\BeforePageDisplay {
 
 	protected function doProcess() {
@@ -9,7 +11,8 @@ class AddResources extends \BlueSpice\Hook\BeforePageDisplay {
 		$this->out->addModules( 'ext.bluespice.multiUpload' );
 		$titles = [];
 		$upload = $this->getServices()->getSpecialPageFactory()->getPage( 'Upload' );
-		if ( $upload ) {
+		$config = MediaWikiServices::getInstance()->getConfigFactory()->makeConfig( 'bsg' );
+		if ( $upload && !$config->get( 'MUPPreventIntegrateMultiUpload' ) ) {
 			$title = $this->getServices()->getSpecialPageFactory()->getLocalNameFor( 'Upload' );
 			$titles[] = [
 				'ns' => NS_SPECIAL,
