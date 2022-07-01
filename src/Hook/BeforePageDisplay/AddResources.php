a6dfa31b1850467138c1963955cf83818ca3926f
miriam schlindwein <schlindwein@hallowelt.com>
2022-07-01 09:08:11 +0200

    Fix config variable
    ERM29003
    
    [4.2.x]
    
    Change-Id: I48d6e805ae9d81cd11fc71461c66c42bc938a5f1

--- src/Hook/BeforePageDisplay/AddResources.php
+++ src/Hook/BeforePageDisplay/AddResources.php
@@ -9,7 +9,8 @@ class AddResources extends \BlueSpice\Hook\BeforePageDisplay {
 		$this->out->addModules( 'ext.bluespice.multiUpload' );
 		$titles = [];
 		$upload = $this->getServices()->getSpecialPageFactory()->getPage( 'Upload' );
-		if ( $upload ) {
+		$config = MediaWikiServices::getInstance()->getConfigFactory()->makeConfig( 'bsg' );
+		if ( $upload && !$config->get( 'MultiUploadIntegrateWithUploadLink' ) ) {
 			$title = $this->getServices()->getSpecialPageFactory()->getLocalNameFor( 'Upload' );
 			$titles[] = [
 				'ns' => NS_SPECIAL,
